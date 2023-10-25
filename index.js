0fd581db1742149133280669c20bdfd3f3af28a8
elmfer <elmfer11@outlook.com>
2023-10-24 21:31:26 -0500

    validate brand name from user

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const inquirer = require('inquirer');
 
+function isBrandValid(brandName) {
+  if(brandName.length === 0)
+    return "Brand name must have at least one character";
+
+  if(brandName.length > 3)
+    return "Brand name must be less the three chraracters";
+
+  return true;
+}
+
 const Questions = [
   {
     message: "Name of your brand",
     type: "input",
-    name: "brand"
+    name: "brand",
+    validate: isBrandValid
   }
 ];
 
