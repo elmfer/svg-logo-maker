ebd55fba14090e75992fcdebe3eacbe9502598e1
elmfer <elmfer11@outlook.com>
2023-10-24 21:25:32 -0500

    test jest and inquirer

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+const inquirer = require('inquirer');
+
+const Questions = [
+  {
+    message: "Name of your brand",
+    type: "input",
+    name: "brand"
+  }
+];
+
+function inquireUser() {
+  return inquirer.prompt(Questions);
+}
+
+function init() {
+  inquireUser().then((answers) => console.log(answers));
+}
+
+init();
